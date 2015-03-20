7fcb0777dcd121ba98050db46636b8e9d1059cad
PabloFer13 <pablofer.melendez@gmail.com>
2015-03-20 15:35:02 -0500

    Create leerForm.js

---
+++ leerForm.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+function checaDatos() {
+	window.onload
+	var telefono = document.getElementsByType("text").value;
+	var Servicios = document.getElementByType("checkbox");
+	print("hola mundo\nHola")
+}
+/**/
