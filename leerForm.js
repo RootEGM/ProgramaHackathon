b1d27b71462585ed9d9f82d7a28b7a60b61021ef
PabloFer13 <pablofer.melendez@gmail.com>
2015-03-20 18:40:54 -0500

    Update leerForm.js

--- leerForm.js
+++ leerForm.js
@@ -1,50 +1,7 @@
-// JavaScript Document
-function checaDatos() {
-	window.onload
-	var telefono = document.getElementsByType("text").value;
-	var Servicios = document.getElementByType("checkbox");
-	print("hola mundo\nHola")
-}
-/**/
+<?php
+	$Nombre = $_POST['celular'];
+	$Comentarios = $_POST['comment'];
 
-<!DOCTYPE html>
-<html>
-<>
-	<body>
-		<form  action="." method="input">
-			<p>
-				Nombre: 
-			<br/>
-				<input type="text" name="celular" autofocus required/>
-			</br>
-			</p>
-		</form>
-
-		<form  method="checkboxes">
-		Que servicio requiere?
-		<br/>
-				<input id="servicios" name="servicio_1" type="checkbox" />Policia
-			<br/>
-				<input id="servicios" name="servicio_2" type="checkbox"/>Transito
-			<br/>
-				<input id="servicios" name="servicio_3" type="checkbox" />Ambulancia
-			<br/>
-				<input id="servicios" name="servicio_4" type="checkbox"/>Bomberos
-		</form>
-
-		<form  action="." method="input">
-			<p>
-			Informacion adicional:
-			</br>
-			<input id="comentario" name="comment" type="text"/>
-			</p>		
-		</form>
-
-		<form>
-			<div>
-  				<input id="boton" type="button" value="Enviar informacion" onclik="checaDatos();" />
-			</div>
- 		</form>
-          
-	</body>
-</html>
+	echo "El Nombre es:".$Nombre."</br>";
+	echo "Comentarios:".$Comentarios."</br>";
+?>
