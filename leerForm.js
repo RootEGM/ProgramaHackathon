670f7eee288e446ad2f8b1d107618c77bccf9392
RubenAldecua <140300686@ucaribe.edu.mx>
2015-03-20 15:17:23 -0600

    Update leerForm.js

--- leerForm.js
+++ leerForm.js
@@ -6,3 +6,45 @@ function checaDatos() {
 	print("hola mundo\nHola")
 }
 /**/
+
+<!DOCTYPE html>
+<html>
+<>
+	<body>
+		<form  action="." method="input">
+			<p>
+				Nombre: 
+			<br/>
+				<input type="text" name="celular" autofocus required/>
+			</br>
+			</p>
+		</form>
+
+		<form  method="checkboxes">
+		Que servicio requiere?
+		<br/>
+				<input id="servicios" name="servicio_1" type="checkbox" />Policia
+			<br/>
+				<input id="servicios" name="servicio_2" type="checkbox"/>Transito
+			<br/>
+				<input id="servicios" name="servicio_3" type="checkbox" />Ambulancia
+			<br/>
+				<input id="servicios" name="servicio_4" type="checkbox"/>Bomberos
+		</form>
+
+		<form  action="." method="input">
+			<p>
+			Informacion adicional:
+			</br>
+			<input id="comentario" name="comment" type="text"/>
+			</p>		
+		</form>
+
+		<form>
+			<div>
+  				<input id="boton" type="button" value="Enviar informacion" onclik="checaDatos();" />
+			</div>
+ 		</form>
+          
+	</body>
+</html>
