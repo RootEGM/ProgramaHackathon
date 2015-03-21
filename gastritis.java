a6291054cee4dfe1a708b56a151141bcef2f4560
PabloFer13 <pablofer.melendez@gmail.com>
2015-03-21 15:37:30 -0500

    Update gastritis.java

--- gastritis.java
+++ gastritis.java
@@ -15,7 +15,7 @@ public class gastritis extends ActionBarActivity {
     }
 
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_gastritis, menu);
@@ -35,5 +35,6 @@ public class gastritis extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
+
