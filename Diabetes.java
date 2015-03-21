0750b615c1b58c57c60c632b352d829770064dff
PabloFer13 <pablofer.melendez@gmail.com>
2015-03-21 15:36:45 -0500

    Update Diabetes.java

--- Diabetes.java
+++ Diabetes.java
@@ -15,7 +15,7 @@ public class Diabetes extends ActionBarActivity {
     }
 
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_diabetes, menu);
@@ -35,5 +35,6 @@ public class Diabetes extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
+
